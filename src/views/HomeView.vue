7199f2ce967400f1a5297e5934bf1a6795dfd80f
Minzza <Minzza>
2023-02-06 23:23:28 +0900

    vuetify 적용

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-icon icon="mdi-vuetify"></v-icon>
+  <hello-world />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+  },
+});
+</script>
