a4faaa86dc31e9a2d7dbc763b4899ac20981e669
minzza <cmjasdf@gmail.com>
2023-02-26 20:02:54 +0900

    메인 레이아웃 완성

---
+++ src/components/MainContent.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card-text>
+    <v-list>
+      <v-list-item
+        v-for="(item, index) in items"
+        :key="index"
+        class="pa-3 border-b-sm"
+      >
+        <div class="mb-2">
+          <v-list-item-title class="d-inline-flex">
+            <router-link :to="'/userts/' + item.id + '/activity'">
+              {{ item.id }}
+            </router-link>
+          </v-list-item-title>
+          ·
+          <v-list-item-title class="d-inline-flex">
+            {{ item.date }}
+          </v-list-item-title>
+          ·
+          <div class="d-inline-flex">
+            <div class="d-inline-flex align-center">
+              <font-awesome-icon icon="fa-eye" />
+              <span class="font-medium ml-1">{{ item.views }}</span>
+              <span class="sr-only">views</span>
+            </div>
+            <div class="d-inline-flex align-center ml-1">
+              <font-awesome-icon icon="fa-regular fa-comment" />
+              <span class="font-medium ml-1">{{ item.comments }}</span>
+              <span class="sr-only">comments</span>
+            </div>
+          </div>
+        </div>
+        <v-list-item-subtitle style="font-size: 18px">
+          <router-link :to="'/question/' + item.contentId">
+            {{ item.contentTitle }}
+          </router-link>
+        </v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+  </v-card-text>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    items: {
+      type: Array,
+      require: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,13 +4,13 @@
       <v-col cols="6">
         <v-card style="height: 50vh">
           <v-card-title>Q&A</v-card-title>
-          <v-card-text> 내용을 입력하세요. </v-card-text>
+          <MainContent :items="qnas" />
         </v-card>
       </v-col>
       <v-col cols="6">
         <v-card style="height: 50vh">
           <v-card-title>커뮤니티</v-card-title>
-          <v-card-text> 내용을 입력하세요. </v-card-text>
+          <MainContent :items="communitys" />
         </v-card>
       </v-col>
     </v-row>
@@ -18,13 +18,13 @@
       <v-col cols="6">
         <v-card style="height: 50vh">
           <v-card-title>WEEKLY BEST</v-card-title>
-          <v-card-text> 내용을 입력하세요. </v-card-text>
+          <MainContent :items="communitys" />
         </v-card>
       </v-col>
       <v-col cols="6">
         <v-card style="height: 50vh">
           <v-card-title>EDITOR'S CHOICE</v-card-title>
-          <v-card-text> 내용을 입력하세요. </v-card-text>
+          <MainContent :items="communitys" />
         </v-card>
       </v-col>
     </v-row>
@@ -32,8 +32,100 @@
 </template>
 
 <script>
+import MainContent from '@/components/MainContent.vue';
+
 export default {
-  name: 'MainPage',
+  components: { MainContent },
+  setup() {
+    const search = '';
+    const qnas = [
+      {
+        id: '오구패밀리',
+        date: '약 10시간 전',
+
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+      {
+        id: '오구패밀리2',
+        date: '약 11시간 전',
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+      {
+        id: '오구패밀리2',
+        date: '약 11시간 전',
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+      {
+        id: '오구패밀리2',
+        date: '약 11시간 전',
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+      {
+        id: '오구패밀리2',
+        date: '약 11시간 전',
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+    ];
+    const communitys = [
+      {
+        id: '오구패밀리',
+        date: '약 10시간 전',
+
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+      {
+        id: '오구패밀리2',
+        date: '약 11시간 전',
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+      {
+        id: '오구패밀리2',
+        date: '약 11시간 전',
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+      {
+        id: '오구패밀리2',
+        date: '약 11시간 전',
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+      {
+        id: '오구패밀리2',
+        date: '약 11시간 전',
+        contentTitle: '질문합니다.',
+        contentId: '',
+        views: '0',
+        comments: '0',
+      },
+    ];
+    return { search, qnas, communitys };
+  },
 };
 </script>
 
